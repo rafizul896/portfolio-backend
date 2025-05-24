6de709ff0294cffc1ff45fb3db5d30566a36d0fe
rafizul896 <rafizulislam896@gmail.com>
2025-05-24 21:24:51 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000","https://rafizul-portfolio-dashboard.vercel.app"],
     credentials: true,
   })
 );
